22418fc92ffe7d54b0d4b5d3e378875af56fb52c
Douglas-leonardo <douglas.bsilva@contmatic.com.br>
2025-05-02 19:32:25 -0300

    Doceria teste

--- meu-projeto-angular/src/app/app.component.ts
+++ meu-projeto-angular/src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { DoceriaComponent } from './doceria/doceria.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ DoceriaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
